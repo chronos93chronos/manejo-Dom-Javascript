539ffd340b90e7f2a474c233cef802b4162cfc55
ALEXANDER HERNANDEZ <111362778+chronos93chronos@users.noreply.github.com>
2023-04-03 18:50:15 -0300

    Add files via upload
    iniciando curso

---
+++ app.js
@@ -0,0 +1,15 @@
+//CURSO DE MANEJO DEL DOM
+//------ETIQUETAS------
+// 1) document = hace referencia al documento o pagina
+// 2) document.getElementById = obtiene un elemento segun su ID
+// 3) document.getElementsByTagName = obtener un elemento segun su nombre de etiqueta
+// 4) document.getElementsByClassName = obtiene un elemento segun su CLASS 
+// 5) document.querySelector = realiza una busqueda de los elementos del documento en funcion de las ESPECIFICACIONES QUE ENTREGAMOS (MIENTRAS MAS ESPECIFICO MEJOR)
+
+
+
+
+//const hermanos = document.querySelector('ul.listas-cursos > li.list') 
+//console.log(hermanos.nextElementSibling);
+ 
+
