5abeff9fbbe9ff94c47278495bf1873cf6da0d47
ALEXANDER HERNANDEZ <111362778+chronos93chronos@users.noreply.github.com>
2023-04-04 09:21:56 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -5,11 +5,36 @@
 // 3) document.getElementsByTagName = obtener un elemento segun su nombre de etiqueta
 // 4) document.getElementsByClassName = obtiene un elemento segun su CLASS 
 // 5) document.querySelector = realiza una busqueda de los elementos del documento en funcion de las ESPECIFICACIONES QUE ENTREGAMOS (MIENTRAS MAS ESPECIFICO MEJOR)
+// 6) document.querySelectorAll = trae todos los elementos que se ESPECIFICA (ejemplo ('img'))
+
+
+
+//primer nodo padre-hijo
+//SE CREA UN NODO
+const padre = document.querySelector('h2');
+console.log(padre);
+// SE CREA OTRO
+ const nuevo_nodo = document.createElement('div');
+ padre.appendChild(nuevo_nodo);
+
+ //se creamos contenido al nuevo nodo
+ const texto = document.createTextNode('viva chile')
+//DONDE SE  DEPOSITA EL TEXTO
+ nuevo_nodo.appendChild(texto);
+
+const final = document.querySelector('div.caja-contenedora');
+console.log(final)
+
+//juego
+
+let alexander =prompt();
+let rival = Math.floor( Math.random());
+alert(alexander)
+alert(rival)
 
 
 
 
-//const hermanos = document.querySelector('ul.listas-cursos > li.list') 
-//console.log(hermanos.nextElementSibling);
- 
+
+
 
